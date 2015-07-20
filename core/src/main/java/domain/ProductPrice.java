5d976a9a0a5f7c104819197d30610c817be5cbd2
pdp <peter.de.permentier@gmail.com>
2015-07-21 00:10:47 +0200

    added productprice

---
+++ core/src/main/java/domain/ProductPrice.java
@@ -0,0 +1,62 @@
+package domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PRODUCT_PRICE")
+public class ProductPrice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "CODE")
+    private String code;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Lob
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product code [%s], name [%s], description [%s]", code, name, description);
+    }
+
+
+}
