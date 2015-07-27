8b6c03b4f962e713cafbcc3305dec84bfc957d41
pdp <peter.de.permentier@gmail.com>
2015-07-27 22:39:18 +0200

    entitylistener

--- core/src/main/java/be/oysterchain/shop/domain/AbstractDomainObject.java
+++ core/src/main/java/be/oysterchain/shop/domain/AbstractDomainObject.java
@@ -1,5 +1,7 @@
 package be.oysterchain.shop.domain;
 
+import be.oysterchain.shop.domain.be.oysterchain.shop.listener.BaseEntityListener;
+
 import javax.persistence.*;
 import java.util.Date;
 
@@ -8,6 +10,7 @@ import java.util.Date;
  * 21.07.15
  */
 @MappedSuperclass
+@EntityListeners(BaseEntityListener.class)
 public abstract class AbstractDomainObject {
 
     @Id
@@ -15,6 +18,68 @@ public abstract class AbstractDomainObject {
     @Column(name = "ID")
     private Long id;
 
+    @Version
+    private Integer version;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "CREATED_ON")
+    private Date createdOn;
+
+/*    @Column(name = "CREATED_BY")
+    private String createdBy;*/
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "LASTMODIFIED_ON")
+    private Date lastModifiedOn;
+
+/*    @Column(name = "LASTMODIFIED_BY")
+    private String lastModifiedBy;
+
+    @Column(name = "creator_user_id")
+    private Long creatorUserId;
+
+    @Column(name = "modification_user_id")
+    private Long modificationUserId;*/
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+/*    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }*/
+
+    public Date getLastModifiedOn() {
+        return lastModifiedOn;
+    }
+
+    public void setLastModifiedOn(Date lastModifiedOn) {
+        this.lastModifiedOn = lastModifiedOn;
+    }
+
+/*    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }*/
 
     public Long getId() {
         return id;
@@ -23,4 +88,6 @@ public abstract class AbstractDomainObject {
     public void setId(Long id) {
         this.id = id;
     }
+
+
 }
