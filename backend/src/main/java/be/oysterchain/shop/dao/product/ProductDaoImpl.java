a0d9ff0e23d890e7e9a669010c05a3d47205f676
peterdp <peter.de.permentier@gmail.com>
2015-08-02 22:18:41 +0200

    replace sessionfactory by entitymanager

--- backend/src/main/java/be/oysterchain/shop/dao/product/ProductDaoImpl.java
+++ backend/src/main/java/be/oysterchain/shop/dao/product/ProductDaoImpl.java
@@ -1,32 +1,26 @@
 package be.oysterchain.shop.dao.product;
 
+import be.oysterchain.shop.dao.common.EntityDao;
 import be.oysterchain.shop.domain.product.Product;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public class ProductDaoImpl implements ProductDao {
 
     @Autowired
-    private SessionFactory sessionFactory;
+    private EntityDao entityDao;
 
     public ProductDaoImpl() {
 
     }
 
     @Override
+    @Transactional
     public void save(Product product) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.persist(product);
-        transaction.commit();
-        session.close();
+        entityDao.persist(product);
     }
 
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
 }
